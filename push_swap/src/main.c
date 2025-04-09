5035efbefa332623deb2abfe69e42cc192329c05
Santiago Alarcon Sanchez <saalarco@c2r10s2.42madrid.com>
2025-04-09 08:32:25 +0200

    folder structure for order few

--- push_swap/src/main.c
+++ push_swap/src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: saalarco <saalarco@student.42.fr>          +#+  +:+       +#+        */
+/*   By: saalarco <saalarco@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 19:38:21 by saalarco          #+#    #+#             */
-/*   Updated: 2025/03/28 18:06:23 by saalarco         ###   ########.fr       */
+/*   Updated: 2025/04/09 08:16:56 by saalarco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,29 +15,31 @@
 int	main(int argc, char **argv)
 {
 	t_list	*a;
-	// t_list *b;
+	t_list *b;
+	t_stacks    *stacks;
+
 	char	**input;
 	char	*str;
 
 	a = NULL;
-	// b = NULL;
+	b = NULL;
 	input = NULL;
 	if (argc < 2 || (argc == 2 && !argv[1][0]))
+		return (0);
+	stacks = (t_stacks *)malloc(sizeof(t_stacks));
+    if (!stacks)
 		return (0);
 	str = ft_join_spaces(argv, argc);
 	input = ft_split_ps(str);
 	free(str);
 	if (input)
-	{
-		a = valid_input_and_list(input);
-		if (a)
-			printf("OK\n");
-	}
-    // here we call order list and print moves function
-	if (a)
-		ft_lstclear(&a, free);
-	else
+		stacks->a_head = valid_input_and_list(input);
+	stacks->b_head = b;
+	if (!(stacks->a_head))
 		print_error();
+	else
+		ft_lstclear(&(stacks->a_head), free);
 	free_argv(input);
+    free(stacks);
 	return (0);
 }
